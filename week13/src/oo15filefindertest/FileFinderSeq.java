033522d2df344ef3d1d0aa25ac928d7d6a938366
lisatostrams <lmc.tostrams@student.ru.nl>
2017-05-16 14:42:39 +0200

    start week 13

---
+++ week13/src/oo15filefindertest/FileFinderSeq.java
@@ -0,0 +1,37 @@
+package oo15filefindertest;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ *
+ * @author pieterkoopman
+ */
+public class FileFinderSeq {
+    private final File rootDir;
+    
+    public FileFinderSeq(String root) throws IOException {
+        rootDir = new File(root);
+        if (!(rootDir.exists() && rootDir.isDirectory())) {
+            throw new IOException(root + " is not a directory");
+        }
+    }
+    
+    public void findFile(String file) {
+        find(rootDir, file);
+    }
+    
+    private void find (File rootDir, String fileName) {
+        File [] files = rootDir.listFiles();
+        if (files != null) {
+            for (File file: files) {
+                if (file.getName().equals(fileName)) {
+                    System.out.println("Found at: " + file.getAbsolutePath());
+                    return;
+                } else if (file.isDirectory()) {
+                    find(file, fileName);
+                }
+            }
+        }
+    }
+}
