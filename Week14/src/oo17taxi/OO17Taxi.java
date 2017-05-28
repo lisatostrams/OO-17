0fc773bcbe615907768020000c1b7b29e5490b2c
lisatostrams <lmc.tostrams@student.ru.nl>
2017-05-28 11:48:07 +0200

    added trace

--- Week14/src/oo17taxi/OO17Taxi.java
+++ Week14/src/oo17taxi/OO17Taxi.java
@@ -11,13 +11,154 @@ package oo17taxi;
 public class OO17Taxi {
 
   public static void main(String[] args) {
-    //Simulation sim = new Simulation();
+
     Simulator sim = new Simulator();
-    /**
-    while (! sim.ended()) {
-      sim.step();
-    }
-    * */
-    //sim.showStatistics();
+
   }
 }
+
+
+/**
+ * Taxi 1 created
+Taxi 2 created
+Taxi 3 created
+Taxi 4 created
+68 passengers arrived at station
+Taxi 1 takes 4 passengers
+Taxi 2 takes 4 passengers
+Taxi 3 takes 7 passengers
+Taxi 4 takes 7 passengers
+Taxi 1 takes 4 passengers
+Taxi 2 takes 4 passengers
+Taxi 4 takes 7 passengers
+Taxi 3 takes 7 passengers
+Taxi 2 takes 4 passengers
+Taxi 1 takes 4 passengers
+75 passengers arrived at station
+Taxi 3 takes 7 passengers
+Taxi 4 takes 7 passengers
+Taxi 1 takes 2 passengers
+Taxi 4 takes 7 passengers
+Taxi 3 takes 7 passengers
+Taxi 1 takes 4 passengers
+Taxi 3 takes 7 passengers
+Taxi 4 takes 7 passengers
+Taxi 1 takes 4 passengers
+Taxi 4 takes 7 passengers
+Taxi 3 takes 7 passengers
+Taxi 1 takes 4 passengers
+Taxi 3 takes 7 passengers
+Taxi 4 takes 7 passengers
+68 passengers arrived at station
+Taxi 1 takes 4 passengers
+Taxi 3 takes 3 passengers
+Taxi 4 takes 7 passengers
+Taxi 1 takes 4 passengers
+Taxi 4 takes 7 passengers
+Taxi 3 takes 7 passengers
+Taxi 1 takes 4 passengers
+Taxi 4 takes 7 passengers
+Taxi 3 takes 7 passengers
+Taxi 1 takes 4 passengers
+Taxi 4 takes 7 passengers
+71 passengers arrived at station
+Taxi 3 takes 7 passengers
+Taxi 1 takes 4 passengers
+Taxi 4 takes 3 passengers
+Taxi 3 takes 7 passengers
+Taxi 1 takes 4 passengers
+Taxi 4 takes 7 passengers
+Taxi 3 takes 7 passengers
+Taxi 1 takes 4 passengers
+Taxi 4 takes 7 passengers
+Taxi 3 takes 7 passengers
+Taxi 1 takes 4 passengers
+Taxi 4 takes 7 passengers
+79 passengers arrived at station
+Taxi 3 takes 7 passengers
+Taxi 1 takes 4 passengers
+Taxi 4 takes 6 passengers
+Taxi 3 takes 7 passengers
+Taxi 1 takes 4 passengers
+Taxi 4 takes 7 passengers
+Taxi 3 takes 7 passengers
+Taxi 1 takes 4 passengers
+Taxi 4 takes 7 passengers
+Taxi 3 takes 7 passengers
+Taxi 1 takes 4 passengers
+Taxi 4 takes 7 passengers
+Taxi 3 takes 7 passengers
+62 passengers arrived at station
+Taxi 1 takes 4 passengers
+Taxi 4 takes 7 passengers
+Taxi 3 takes 7 passengers
+Taxi 1 takes 4 passengers
+Taxi 4 takes 7 passengers
+Taxi 3 takes 7 passengers
+Taxi 1 takes 4 passengers
+Taxi 4 takes 7 passengers
+Taxi 3 takes 7 passengers
+Taxi 1 takes 4 passengers
+Taxi 4 takes 7 passengers
+74 passengers arrived at station
+Taxi 3 takes 7 passengers
+Taxi 1 takes 4 passengers
+Taxi 4 takes 4 passengers
+Taxi 3 takes 7 passengers
+Taxi 1 takes 4 passengers
+Taxi 4 takes 7 passengers
+Taxi 3 takes 7 passengers
+Taxi 1 takes 4 passengers
+Taxi 4 takes 7 passengers
+Taxi 3 takes 7 passengers
+Taxi 1 takes 4 passengers
+Taxi 4 takes 7 passengers
+Taxi 3 takes 7 passengers
+63 passengers arrived at station
+Taxi 1 takes 4 passengers
+Taxi 4 takes 7 passengers
+Taxi 3 takes 2 passengers
+Taxi 1 takes 4 passengers
+Taxi 4 takes 7 passengers
+Taxi 3 takes 7 passengers
+Taxi 1 takes 4 passengers
+Taxi 4 takes 7 passengers
+Taxi 3 takes 7 passengers
+Taxi 1 takes 4 passengers
+Taxi 4 takes 7 passengers
+77 passengers arrived at station
+Taxi 3 takes 7 passengers
+Taxi 1 takes 4 passengers
+Taxi 4 takes 5 passengers
+Taxi 3 takes 7 passengers
+Taxi 1 takes 4 passengers
+Taxi 4 takes 7 passengers
+Taxi 3 takes 7 passengers
+Taxi 1 takes 4 passengers
+Taxi 4 takes 7 passengers
+Taxi 3 takes 7 passengers
+Taxi 1 takes 4 passengers
+Taxi 4 takes 7 passengers
+Taxi 3 takes 7 passengers
+75 passengers arrived at station
+Taxi 1 takes 4 passengers
+Taxi 4 takes 7 passengers
+Taxi 3 takes 5 passengers
+Taxi 1 takes 4 passengers
+Taxi 4 takes 7 passengers
+Taxi 3 takes 7 passengers
+Taxi 1 takes 4 passengers
+Taxi 4 takes 7 passengers
+Taxi 3 takes 7 passengers
+Taxi 1 takes 4 passengers
+Taxi 4 takes 7 passengers
+Taxi 3 takes 7 passengers
+Taxi 1 takes 4 passengers
+Taxi 4 takes 7 passengers
+Taxi 3 takes 7 passengers
+Taxi 1 takes 3 passengers
+All persons have been transported
+Total transport time in this simulation:328
+Total number of train travelers: 712
+Total number of persons transported in this simulation: 712
+ */
